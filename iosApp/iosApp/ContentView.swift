ee6091d7cdef46b96723267484fa54595ead8818
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2024-11-27 15:56:13 +0000

    [IOS] Pipe location into view model

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,8 +1,10 @@
 import SwiftUI
 import Shared
+import KMPObservableViewModelSwiftUI
 @_spi(Experimental) import MapboxMaps
 
 struct ContentView: View {
+    @StateViewModel var viewModel = AppViewModel()
     @State private var showContent = false
     @State var viewport: Viewport = .followPuck(zoom: 16)
     @State var locationProvider: AppleLocationProvider = AppleLocationProvider()
@@ -31,8 +33,12 @@ struct ContentView: View {
                     Puck2D(bearing: .heading).showsAccuracyRing(true)
                 }.onAppear {
                     // Start listening for location updates when map is ready
-                    locationObserver = locationProvider.onLocationUpdate.observe { location in
-                        print("Location  \(location[0].coordinate)")
+                    locationObserver = locationProvider.onLocationUpdate.observe { locations in
+                        // TODO Move mapping out of view 
+                        let mapped = locations.map { (location) -> Shared.Location in
+                            Shared.Location(lat: location.coordinate.latitude, lng: location.coordinate.longitude)
+                        }
+                        viewModel.onLocationChange(locations: mapped)
                     }
                 }.onDisappear{
                     locationObserver?.cancel()
