869c7f79c15c9721e532eee4cd4c13f627bf387c
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2024-11-30 11:12:32 +0000

    [Android&IOS] Remove demo code

--- composeApp/src/androidMain/kotlin/uk/co/oliverdelange/location_alarm/screens/App.kt
+++ composeApp/src/androidMain/kotlin/uk/co/oliverdelange/location_alarm/screens/App.kt
@@ -1,24 +1,18 @@
 package uk.co.oliverdelange.location_alarm.screens
 
 import Greeting
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mapbox.geojson.Point
@@ -59,18 +53,9 @@ fun App(viewmodel: AppViewModel = viewModel()) {
     val state by viewmodel.state.collectAsStateWithLifecycle()
 
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(R.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            val greeting = remember { Greeting().greet() }
+            Text("Compose: $greeting")
             MapboxMap(
                 Modifier.fillMaxSize(),
                 mapViewportState = MapViewportState().apply {
--- iosApp/iosApp/ui/ContentView.swift
+++ iosApp/iosApp/ui/ContentView.swift
@@ -5,46 +5,29 @@ import KMPObservableViewModelSwiftUI
 
 struct ContentView: View, LocationService.LocationServiceDelegate {
     @StateViewModel var viewModel = AppViewModel()
-    @State private var showContent = false
     @State var viewport: Viewport = .followPuck(zoom: 16)
     @State var locationService: LocationService = LocationService()
-
+    
     var body: some View {
         VStack {
-            Text("\(viewModel.state.usersLocation)")
-            Button("Click me!") {
-                withAnimation {
-                    showContent = !showContent
-                }
-            }
-            
-            if showContent {
-                VStack(spacing: 16) {
-                    Image(systemName: "swift")
-                        .font(.system(size: 200))
-                        .foregroundColor(.accentColor)
-                    Text("SwiftUI: \(Greeting().greet())")
-                }
-                .transition(.move(edge: .top).combined(with: .opacity))
-            }
+            Text("SwiftUI: \(Greeting().greet())")
             
             MapReader { map in
                 Map(viewport: $viewport)
                 {
                     Puck2D(bearing: .heading).showsAccuracyRing(true)
-                   if let geofence = viewModel.state.geoFenceLocation {
-                       let circle = createCirclePolygon(center: geofence.toCLLocationCoordinate2D(), radius: Double(viewModel.state.perimeterRadiusMeters))
-                       var featureCollection: FeatureCollection = FeatureCollection(features: [Feature(geometry: .polygon(circle))])
-
-                       GeoJSONSource(id: MapboxIDs.shared.SOURCE_GEOFENCE)
-                           .data(.featureCollection(featureCollection))
-                       FillLayer(id: MapboxIDs.shared.LAYER_GEOFENCE_FILL, source: MapboxIDs.shared.SOURCE_GEOFENCE)
-                           .fillColor(UIColor(named: "GeofenceLine")!)
-                           .fillOpacity(0.3)
-                       LineLayer(id: MapboxIDs.shared.LAYER_GEOFENCE_LINE, source: MapboxIDs.shared.SOURCE_GEOFENCE)
-                           .lineWidth(5.0)
-                           .lineColor(UIColor(named: "GeofenceLine")!)
-                   }
+                    if let geofence = viewModel.state.geoFenceLocation {
+                        let circle = createCirclePolygon(center: geofence.toCLLocationCoordinate2D(), radius: Double(viewModel.state.perimeterRadiusMeters))
+                        
+                        GeoJSONSource(id: MapboxIDs.shared.SOURCE_GEOFENCE)
+                            .data(.feature(Feature(geometry: .polygon(circle))))
+                        FillLayer(id: MapboxIDs.shared.LAYER_GEOFENCE_FILL, source: MapboxIDs.shared.SOURCE_GEOFENCE)
+                            .fillColor(UIColor(named: "GeofenceLine")!)
+                            .fillOpacity(0.3)
+                        LineLayer(id: MapboxIDs.shared.LAYER_GEOFENCE_LINE, source: MapboxIDs.shared.SOURCE_GEOFENCE)
+                            .lineWidth(5.0)
+                            .lineColor(UIColor(named: "GeofenceLine")!)
+                    }
                 }.onAppear {
                     logger.debug("Map did appear")
                     locationService.checkLocationPermissionsAndStartListening()
@@ -65,7 +48,7 @@ struct ContentView: View, LocationService.LocationServiceDelegate {
             locationService.delegate = self
         }
     }
-
+    
     func onLocationUpdate(locations: Array<Shared.Location>) {
         viewModel.onLocationChange(locations: locations)
     }
