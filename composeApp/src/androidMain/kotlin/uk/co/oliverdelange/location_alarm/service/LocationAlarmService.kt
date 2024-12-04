977bdcdff3c745c7d5c0a28b0b24fbc2cd599408
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2024-12-04 17:48:16 +0000

    [Android] Play alarm sound in service

--- composeApp/src/androidMain/kotlin/uk/co/oliverdelange/location_alarm/service/LocationAlarmService.kt
+++ composeApp/src/androidMain/kotlin/uk/co/oliverdelange/location_alarm/service/LocationAlarmService.kt
@@ -3,8 +3,13 @@ package uk.co.oliverdelange.location_alarm.service
 import android.app.Notification
 import android.app.Notification.FOREGROUND_SERVICE_IMMEDIATE
 import android.app.Service
+import android.content.ContentResolver
 import android.content.Intent
 import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.IBinder
 import androidx.core.app.ServiceCompat
 import timber.log.Timber
@@ -14,6 +19,7 @@ import uk.co.oliverdelange.location_alarm.notifications.createAlarmNotificationC
 
 class LocationAlarmService : Service() {
     private val notificationId = 60494
+    private lateinit var alarmPlayer: MediaPlayer
 
     override fun onBind(intent: Intent?): IBinder? {
         return null // We're not allowing binding
@@ -22,13 +28,30 @@ class LocationAlarmService : Service() {
     override fun onCreate() {
         Timber.d("LocationAlarmService onCreate")
         super.onCreate()
+        val alarmUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+            ?: Uri.Builder()
+                .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                .authority(packageName)
+                .appendPath("${R.raw.alarm}")
+                .build()
+        val alarmAudioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+        alarmPlayer = MediaPlayer().apply {
+            setDataSource(this@LocationAlarmService, alarmUri)
+            setAudioAttributes(alarmAudioAttributes)
+            isLooping = true
+            prepare()
+        }
+        alarmPlayer.start()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Timber.d("LocationAlarmService onStartCommand")
         createAlarmNotificationChannel(this)
         val notification = Notification.Builder(this, NOTIFICATION_CHANNEL_ID_MAIN)
-            .setSmallIcon(R.drawable.ic_notification_icon) // Replace with your icon
+            .setSmallIcon(R.drawable.ic_notification_icon)
             .setContentTitle("Location Alarm Active")
             .setContentText("Alarm will sound near your destination")
             .setOngoing(true) // User can't dismiss notification
@@ -44,6 +67,9 @@ class LocationAlarmService : Service() {
     }
 
     override fun onDestroy() {
+        if (alarmPlayer.isPlaying) {
+            alarmPlayer.stop()
+        }
         Timber.d("LocationAlarmService onDestroy")
         super.onDestroy()
     }
