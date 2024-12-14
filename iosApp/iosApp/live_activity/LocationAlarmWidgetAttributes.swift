2801746999d3f46861a52e54a13abcb21b19cc13
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2024-12-14 11:48:30 +0000

    [IOS] Fix duplicated attributes issue

--- iosApp/iosApp/live_activity/LocationAlarmWidgetAttributes.swift
+++ iosApp/iosApp/live_activity/LocationAlarmWidgetAttributes.swift
@@ -1,18 +1,7 @@
-//
-//  LocationAlarmWidgetAttributes.swift
-//  iosApp
-//
-//  Created by Oliver de Lange on 13/12/2024.
-//  Copyright © 2024 orgName. All rights reserved.
-//
-
-
 import ActivityKit
 import Combine
 import Foundation
 
-//FIXME this is duplicated in LocationAlarmWidget
-// Doesn't compile when it doesn't exist in both places
 struct LocationAlarmWidgetAttributes: ActivityAttributes {
     public struct ContentState: Codable, Hashable {
         // Dynamic stateful properties about your activity go here!
