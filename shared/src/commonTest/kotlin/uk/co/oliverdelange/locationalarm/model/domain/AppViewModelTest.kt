0dbfaae0f20a409bc55fd423ded0a7221a313612
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2024-12-19 12:42:31 +0000

    [Test] Unit test passes now. Added coroutine test dispatcher

--- shared/src/commonTest/kotlin/uk/co/oliverdelange/locationalarm/model/domain/AppViewModelTest.kt
+++ shared/src/commonTest/kotlin/uk/co/oliverdelange/locationalarm/model/domain/AppViewModelTest.kt
@@ -1,8 +1,26 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import uk.co.oliverdelange.locationalarm.model.domain.AppViewModel
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class AppViewModelTest {
+
+    @BeforeTest
+    fun before() {
+        Dispatchers.setMain(StandardTestDispatcher())
+    }
+
+    @AfterTest
+    fun after() {
+        Dispatchers.resetMain()
+    }
 
     @Test
     fun onTapAllowLocationPermissions_setsState() {
