49854ea393cf8fdf0981b65eafef60c33f955fdc
Oliver Culley de Lange <olly@oliverdelange.co.uk>
2025-01-07 17:17:29 +0000

    [Android] Re-enable using default alarm sound

--- composeApp/src/androidMain/kotlin/uk/co/oliverdelange/location_alarm/service/LocationAlarmService.kt
+++ composeApp/src/androidMain/kotlin/uk/co/oliverdelange/location_alarm/service/LocationAlarmService.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.os.IBinder
@@ -46,8 +47,8 @@ class LocationAlarmService : Service() {
 
     override fun onCreate() {
         SLog.d("LocationAlarmService onCreate")
-        val alarmUri: Uri =/* RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-            ?: */Uri.Builder()
+        val alarmUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+            ?: Uri.Builder()
                 .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
                 .authority(packageName)
                 .appendPath("${R.raw.alarm}")
